20ab953842d8ec60bf3e255971723a966266d980
Richard Gregorio <thekrunchyfrog@gmail.com>
2018-02-10 13:56:18 -0500

    added changes for common anode led and method for blanking all

--- RobieEyes.py
+++ RobieEyes.py
@@ -4,10 +4,6 @@ import Adafruit_PCA9685
 class RobieEyes:
     pwmHat = Adafruit_PCA9685.PCA9685()
     pwmHat.set_pwm_freq(1000)
-    
-    pwmHat.set_pwm(0, 0, 1)
-    pwmHat.set_pwm(1, 4096, 1)  #blue
-    pwmHat.set_pwm(2, 0, 1)
 
     def setColor(self, channel, red, green, blue):
         red = self.translate(red)
@@ -19,7 +15,18 @@ class RobieEyes:
         self.pwmHat.set_pwm((channel + 2), green, 1)
 
     def translate(self, value):
+        value = (value - 255) * -1  # convertion for common anode led
         valueScaled = value / 255
         return valueScaled * 4096
 
-eye = RobieEyes()
+    def blankEyes(self):
+        self.pwmHat.set_pwm(0, 4096, 0)
+        self.pwmHat.set_pwm(1, 4096, 0)
+        self.pwmHat.set_pwm(2, 4096, 0)
+
+        self.pwmHat.set_pwm(3, 4096, 0)
+        self.pwmHat.set_pwm(4, 4096, 0)
+        self.pwmHat.set_pwm(5, 4096, 0)
+
+
+eye = RobieEyes().setColor(0, 255, 0, 0)
