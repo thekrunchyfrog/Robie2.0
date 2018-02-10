c45f7595755f98645c250539567f45c9b37e7f1d
Richard Gregorio <thekrunchyfrog@gmail.com>
2018-02-10 14:32:46 -0500

    added endpoints for changing color of eyes and blanking them

--- RobieService.py
+++ RobieService.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import Response
 from flask.ext.cors import CORS, cross_origin
 from RobieLegs import *
+from RobieEyes import *
 
 app = Flask(__name__)
 
@@ -20,6 +21,44 @@ def api_Legs():
         resp = Response()
         resp.status_code = 200
         resp.headers['server'] = ""
+        return resp
+
+    else:
+        resp = Response("Try Again :-(", status=415, mimetype='text/plain')
+        resp.headers['server'] = ""
+        return resp
+
+
+@app.route('/robie/eyes', methods=['POST'])
+@cross_origin(origin='192.168.1.172', headers=['Content- Type', 'Authorization'])
+def api_Eyes():
+
+    if request.headers['Content-Type'] == 'application/json':
+        channel = request.json['eye']
+        red = request.json['red']
+        green = request.json['green']
+        blue = request.json[' blue']
+        RobieEyes().setColor(channel, red, green, blue)
+        resp = Response()
+        resp.status_code = 200
+        response.headers['server'] = ""
+        return resp
+
+    else:
+        resp = Response("Try Again :-(", status=415, mimetype='text/plain')
+        resp.headers['server'] = ""
+        return resp
+
+
+@app.route('/robie/eyes/close', methods=['POST'])
+@cross_origin(origin='192.168.1.172', headers=['Content- Type', 'Authorization'])
+def api_Eyes_Close():
+
+    if request.headers['Content-Type'] == 'application/json':
+        RobieEyes().blankEyes()
+        resp = Response()
+        resp.status_code = 200
+        response.headers['server'] = ""
         return resp
 
     else:
